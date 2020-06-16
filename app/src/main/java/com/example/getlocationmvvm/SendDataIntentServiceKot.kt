2a0c2a98bb55fe9d7b93057c1ce91dbf698bdced
baandalon <66965187+baandalon@users.noreply.github.com>
2020-06-16 16:55:51 -0500

    MVVM

--- app/src/main/java/com/example/getlocationmvvm/SendDataIntentServiceKot.kt
+++ app/src/main/java/com/example/getlocationmvvm/SendDataIntentServiceKot.kt
@@ -1,13 +1,11 @@
 package com.example.getlocationmvvm
 
 import android.Manifest
-import android.app.Activity
 import android.app.IntentService
 import android.app.Notification
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Build
 import android.os.Looper
 import android.os.PowerManager
@@ -19,6 +17,9 @@ import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class SendDataIntentServiceKot : IntentService("SendDataIntentServiceKot") {
 
@@ -45,42 +46,48 @@ class SendDataIntentServiceKot : IntentService("SendDataIntentServiceKot") {
         Log.e("intent", "onHandleIntent")
         val input = intent!!.getStringExtra("inputExtra")
         for (i in 0..9) {
-            currentLocation
-            SystemClock.sleep(1000)
+            currentLocation()
+
         }
         wakeLock?.acquire(0)
     }
 
-    //                            fetchAddressFromLatLong(location);
-    private val currentLocation: Location?
-        private get() {
-            val data: Location? = null
-            val locationRequest = LocationRequest()
-            locationRequest.interval = 1000
-            locationRequest.fastestInterval = 500
-            locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return data
-            }
-            LocationServices.getFusedLocationProviderClient(this)
-                    .requestLocationUpdates(locationRequest, object : LocationCallback() {
-                        override fun onLocationResult(locationResult: LocationResult) {
-                            super.onLocationResult(locationResult)
-                            LocationServices.getFusedLocationProviderClient(this@SendDataIntentServiceKot)
-                                    .removeLocationUpdates(this)
-                            if (locationResult != null && locationResult.locations.size > 0) {
-                                val latestLocationIndex = locationResult.locations.size - 1
-                                val latitude = locationResult.locations[latestLocationIndex].latitude
-                                val longitude = locationResult.locations[latestLocationIndex].longitude
-                                Log.e("intent latttt", latitude.toString())
-                                Log.e("intent lonnnn", longitude.toString())
+    fun currentLocation() {
+        GlobalScope.launch {
+            runBlocking {
 
-                                //                            fetchAddressFromLatLong(location);
+                val locationRequest = LocationRequest()
+                locationRequest.interval = 1000
+                locationRequest.fastestInterval = 500
+                locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                if (ActivityCompat.checkSelfPermission(this@SendDataIntentServiceKot, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                                this@SendDataIntentServiceKot, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return@runBlocking
+                }
+                LocationServices.getFusedLocationProviderClient(this@SendDataIntentServiceKot)
+                        .requestLocationUpdates(locationRequest, object : LocationCallback() {
+                            override fun onLocationResult(locationResult: LocationResult) {
+                                super.onLocationResult(locationResult)
+                                LocationServices.getFusedLocationProviderClient(this@SendDataIntentServiceKot)
+                                        .removeLocationUpdates(this)
+                                if (locationResult != null && locationResult.locations.size > 0) {
+                                    val latestLocationIndex = locationResult.locations.size - 1
+                                    val latitude = locationResult.locations[latestLocationIndex].latitude
+                                    val longitude = locationResult.locations[latestLocationIndex].longitude
+                                    Log.e("intent latttt", latitude.toString())
+                                    Log.e("intent lonnnn", longitude.toString())
+
+                                    //                            fetchAddressFromLatLong(location);
+                                }
                             }
-                        }
-                    }, Looper.getMainLooper())
-            return data
+                        }, Looper.getMainLooper())
+
+                SystemClock.sleep(1000)
+            }
+
         }
+
+    }
 
     override fun onDestroy() {
         super.onDestroy()
