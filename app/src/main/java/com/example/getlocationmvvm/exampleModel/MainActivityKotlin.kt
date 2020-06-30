9d3670416652e90cef79d435d89af35737eee8b6
baandalon <66965187+baandalon@users.noreply.github.com>
2020-06-30 18:55:52 -0500

    MVVM

--- app/src/main/java/com/example/getlocationmvvm/exampleModel/MainActivityKotlin.kt
+++ app/src/main/java/com/example/getlocationmvvm/exampleModel/MainActivityKotlin.kt
@@ -1,10 +1,8 @@
 package com.example.getlocationmvvm.exampleModel
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.getlocationmvvm.R
 import com.example.getlocationmvvm.databinding.ActivityMainKotlinBinding
@@ -12,16 +10,15 @@ import com.example.getlocationmvvm.exampleModel.viewmodel.MainViewModel
 
 
 class MainActivityKotlin : AppCompatActivity() {
-    private var viewModel : MainViewModel? = null
+    val viewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_kotlin)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        val binding: ActivityMainKotlinBinding = DataBindingUtil
-                .setContentView(this, R.layout.activity_main_kotlin)
-        viewModel!!.dataModel?.observe(this, Observer {
-            Log.e("data", it.data.name)
-        })
-        viewModel!!.onDataRequest("ExampleMutableLiveDataModel")
+        lifecycle.addObserver(viewModel)
+        DataBindingUtil.setContentView<ActivityMainKotlinBinding>(this,
+                R.layout.activity_main_kotlin).apply {
+            this.lifecycleOwner = this@MainActivityKotlin
+            this.viewModel2 = viewModel
+        }
     }
 }
--- app/src/main/java/com/example/getlocationmvvm/exampleModel/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/getlocationmvvm/exampleModel/viewmodel/MainViewModel.kt
@@ -1,8 +1,7 @@
 package com.example.getlocationmvvm.exampleModel.viewmodel
 
 import android.util.Log
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.example.getlocationmvvm.exampleModel.interfaces.DataInterfaces
 import com.example.getlocationmvvm.exampleModel.model.Action
 import com.example.getlocationmvvm.exampleModel.model.Data
@@ -10,25 +9,28 @@ import com.example.getlocationmvvm.exampleModel.model.ModelStructure
 import com.example.getlocationmvvm.exampleModel.repository.RepoWebDS
 import com.google.firebase.database.DataSnapshot
 
-class MainViewModel: ViewModel(), DataInterfaces{
+class MainViewModel: ViewModel(), LifecycleObserver, DataInterfaces{
 
-    var repo: RepoWebDS? = null
-    var dataModel: MutableLiveData<ModelStructure>? = null
+    var repo = RepoWebDS()
+    var dataModel= MutableLiveData<ModelStructure>()
 
     init {
-        repo = RepoWebDS()
         dataModel = MutableLiveData()
-        repo?.setDataInterfaces(this)
+        repo.setDataInterfaces(this)
     }
     fun onDataRequest(path: String){
-        repo?.getData(path)
+        repo.getData(path)
     }
 
     override fun onDataChanged(data: DataSnapshot) {
         val modelAction: Action = data.child("action").getValue(Action::class.java)!!
         val modelData: Data = data.child("data").getValue(Data::class.java)!!
                 val modelStructure = ModelStructure(modelAction, modelData)
-                dataModel!!.postValue(modelStructure)
+                dataModel.postValue(modelStructure)
     }
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreated(){
+        onDataRequest("ExampleMutableLiveDataModel")
+    }
 }
