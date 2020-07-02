8f825d5fd3ee82c5524a5df6ea9a0f0cc5400f23
baandalon <66965187+baandalon@users.noreply.github.com>
2020-07-02 18:58:10 -0500

    MVVM

--- app/src/main/java/com/example/getlocationmvvm/exampleModel/MainActivityKotlin.kt
+++ app/src/main/java/com/example/getlocationmvvm/exampleModel/MainActivityKotlin.kt
@@ -1,12 +1,14 @@
 package com.example.getlocationmvvm.exampleModel
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.getlocationmvvm.R
 import com.example.getlocationmvvm.databinding.ActivityMainKotlinBinding
 import com.example.getlocationmvvm.exampleModel.viewmodel.MainViewModel
+import com.gurutouchlabs.kenneth.elegantdialog.ElegantDialog
 
 
 class MainActivityKotlin : AppCompatActivity() {
@@ -20,5 +22,31 @@ class MainActivityKotlin : AppCompatActivity() {
             this.lifecycleOwner = this@MainActivityKotlin
             this.viewModel2 = viewModel
         }
+
+    }
+
+    fun start(view: View) {
+        val dialog = ElegantDialog(this)
+                .setTitleIcon(resources.getDrawable( R.drawable.dialog))//Set title icon drawable if your not loading with Glide or Picasso )//Set title icon drawable if your not loading with Glide or Picasso
+                .setTitleIconBackgroundColor(resources.getColor(R.color.topImage)) //Set title icon drawable background color
+                .setBackgroundTopColor(resources.getColor(R.color.white))// Set top color//                .setBackgroundBottomColor(backgroundBottomColor) // Set bottom color
+                .setTitleIconColor(resources.getColor(R.color.white))
+                .setCustomView(R.layout.activity_start_share_location)//Set custom layout
+                .setCornerRadius(10f) //Set dialog corner radius
+                .setCanceledOnTouchOutside(true) // Dismiss on tap outside
+                .show()  // Finally don't forget to call show()
+        dialog.getNegativeButton()!!.visibility = View.INVISIBLE
+        dialog.getPositiveButton()!!.visibility = View.INVISIBLE
+        dialog.getGotItButtonTextView()!!.text = "ACEPTAR"
+        dialog.getGotItButtonIconView()!!.visibility = View.GONE
+        dialog.getNegativeButtonIconView()!!.visibility = View.GONE
+        dialog.getPositiveButtonIconView()!!.visibility = View.GONE
+        dialog.getNegativeButtonTextView()!!.visibility = View.GONE
+        dialog.getPositiveButtonTextView()!!.visibility = View.GONE
+        dialog.getGotItButton()!!.setOnClickListener {
+            dialog.dismiss()
+        }
+        // access your customView
+        val contentView: View? = dialog.getCustomView()
     }
 }
